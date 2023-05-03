cc22e32376eb46cbcd77bf448956963d696128b3
tarequemusa <tareque.web@gmail.com>
2023-05-04 03:58:33 +0600

    index.js file updated for View Recipe Button Data

--- index.js
+++ index.js
@@ -1,29 +1,52 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 const chefsDetails = require('./data/chefsDetails.json');
 const chefRecipe = require('./data/recipes.json');
 
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('The Chefs Cafe Server is Running')
 });
 
 app.get('/chefsdetails', (req, res) => {
+    console.log(chefsDetails);
     res.send(chefsDetails);
 })
-app.get('/recipes', (req, res) => {
-    res.send(chefRecipe);
-})
 
-app.get('/chefsdetails/:id', (req, res) => {
+app.get('./data/chefsdetails/:id', (req, res) => {
     const id = req.params.id;
     console.log(id);
     const selectedChef = chefsDetails.find(n => n.id === id);
     res.send(selectedChef);
 })
 
+app.get('/recipes', (req, res) => {
+    res.send(chefRecipe);
+})
+
+app.get('/recipes/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(id);
+    const selectedRecipe = chefRecipe.find(n => n.recipe_id === id);
+    res.send(selectedRecipe)
+})
+
+app.get('/chefsdetails/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    if(id === 0) {
+        res.send(chefsDetails)
+    }
+    else {
+        const selectedChef = chefsDetails.filter(n => parseInt(n.id) === id);
+        res.send(selectedChef);
+    }
+    console.log(id);
+})
+
 app.listen(port, () => {
-    console.log('The Chefs Cafe API is running on port: ${port}');
+    console.log(`The Chefs Cafe API is running on port: ${ port }`);
 })
